4cb551117a022741832ad688192ad7d0d328febc
gr0vity-dev <iq.cc@pm.me>
2022-10-14 23:44:24 +0200

    test ascending bootstrapper with dockerized 2node network connecting to BETA/LIVE network. stats enabled

---
+++ ascending_bootstrapper/run_test_asc_bootstrap.sh
@@ -0,0 +1,18 @@
+env=beta
+docker_tag=new-bootstrap-$(date '+%Y%m%d')
+build_path=/root/git/nano-workspace/pwo_nano-node
+test_path=./
+
+echo "BUILD nano_node from $build_path with tag '$docker_tag'"
+
+#build the nano node as docker container
+cd $build_path &&  docker build -f docker/node/Dockerfile -t $docker_tag .
+
+#stop any current test
+cd $test_path && docker-compose -f docker-compose.$env.yml down
+#replace the docker tag in the compose file with the latest build
+cd $test_path && sed -i "s/image.*/image: $docker_tag/g" docker-compose.yml
+#remove the  client ledger to start from 0 for each testrun
+cd $test_path/NanoBeta_client/NanoBeta && rm -rf rocksdb && rm -f data.ldb
+#start the nodes and run the test
+cd $test_path && docker-compose -f docker-compose.$env.yml up -d
