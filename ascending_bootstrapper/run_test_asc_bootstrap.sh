386e82c100680d45579c013f72d1a79aac618ea8
gr0vity <gr0vity.dev@gmail.com>
2022-11-14 18:08:52 +0100

    isolated version of the bootstrapping test

--- ascending_bootstrapper/run_test_asc_bootstrap.sh
+++ ascending_bootstrapper/run_test_asc_bootstrap.sh
@@ -1,24 +1,15 @@
 env=beta #or live
-docker_tag=new-bootstrap-$(date '+%Y%m%d')
-build_path=https://github.com/nanocurrency/nano-node\#new-bootstrap
+docker_tag="docker.io\/library\/new-bootstrap_2022-11-14" #escape slashes!
 test_path=. #current folder
-
-echo "BUILD nano_node from $build_path with tag '$docker_tag'"
-
-#build the nano node as docker container
-if [[ $build_path =~ ^\http.* ]]
-then #build directly from git
-  docker build -f docker/node/Dockerfile -t $docker_tag $build_path
-else #build from workspace
-  cd $build_path &&  docker build -f docker/node/Dockerfile -t $docker_tag .
-fi  
-
+prom_exporter_job="nano-tests_ascending_bootstrap"
 
 #stop any current test
 cd $test_path && docker-compose -f docker-compose.$env.yml down
 #replace the docker tag in the compose file with the latest build
-cd $test_path && sed -i "s/image.*/image: $docker_tag/g" docker-compose.yml
+cd $test_path && sed -i "s/image.*/image: $docker_tag/g" docker-compose.$env.yml
 #remove the  client ledger to start from 0 for each testrun
-cd $test_path/NanoBeta_client/NanoBeta && rm -rf rocksdb && rm -f data.ldb
+cd $test_path/client_node/NanoBeta && rm -rf rocksdb && rm -f data.ldb && cd ../../
+#replace prom_exporter job showing the testrun under https://nl-nodestats.bnano.info
+cd $test_path && sed -i "s/--runid.*/--runid ${prom_exporter_job}_${env}/g" docker-compose.$env.yml
 #start the nodes and run the test
 cd $test_path && docker-compose -f docker-compose.$env.yml up -d
